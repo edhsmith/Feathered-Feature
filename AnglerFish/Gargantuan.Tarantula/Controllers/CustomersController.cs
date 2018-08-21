f0ca58c1cdb0741ac5541a2159e4d40a0e283980
Edmund Smith <smithed@ROADS.VIC.GOV.AU>
2018-08-21 11:52:26 +1000

    Add Angular Include feature.

---
+++ AnglerFish/Gargantuan.Tarantula/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Gargantuan.Tarantula.Models;
+
+namespace Gargantuan.Tarantula.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        [HttpGet]
+        public Record[] Customers()
+        {
+            return new Record[]
+            {
+                new Record(name:"Alfreds Futterkiste",country:"Germany")
+                , new Record(name:"Ana Trujillo Emparedados y helados",country:"Mexico")
+            };
+        }
+    }
+}
---
+++ AnglerFish/Gargantuan.Tarantula/Models/Records.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gargantuan.Tarantula.Models
+{
+    public class Record
+    {
+        public Record(string name,string country)
+        {
+            Name = name;
+            Country = country;
+        }
+
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
