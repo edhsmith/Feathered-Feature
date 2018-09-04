b68de1c86053e5f3d6504dce7a4e750cfd29f31b
Edmund Smith <smithed@ROADS.VIC.GOV.AU>
2018-09-04 15:46:54 +1000

    Added MVC Authentication sample.

--- AnglerFish/Gargantuan.Tarantula/Controllers/HomeController.cs
+++ AnglerFish/Gargantuan.Tarantula/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Gargantuan.Tarantula.Controllers
 {
@@ -26,5 +27,39 @@ namespace Gargantuan.Tarantula.Controllers
 
             return View();
         }
+
+
+        #region "Authentication Functions"
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Authenticate(FormCollection body)
+        {
+            //this.User = new System.Security.Principal.GenericPrincipal(new System.Security.Principal.GenericIdentity(body["Name"]),new string[] { });
+            DateTime expire = DateTime.Now.AddMinutes(15);
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, "cookie", DateTime.Now, expire, true, body["Name"]);
+            HttpCookie cookie = new HttpCookie("ChocolateChip");
+            cookie.Value = FormsAuthentication.Encrypt(ticket);
+            Response.Cookies.Add(cookie);
+
+            string redirectUrl = Request.QueryString.AllKeys.Contains("returnUrl") ? Request.QueryString["returnUrl"] : "Index";
+
+
+
+            return new RedirectResult(redirectUrl);
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            HttpCookie cookie = new HttpCookie("ChocolateChip");
+
+            Response.SetCookie(cookie);
+
+            return new RedirectResult("Index");
+        }
+
+        #endregion
+
     }
 }
---
+++ AnglerFish/Gargantuan.Tarantula/Models/AreaAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace Gargantuan.Tarantula.Models
+{
+    public class AreaAuthorizeAttribute: AuthorizeAttribute
+    {
+        public AreaAuthorizeAttribute()
+        {
+
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            //Get Area
+            string area = filterContext.RouteData.DataTokens.Keys.Contains("area")
+                      ? filterContext.RouteData.DataTokens["area"].ToString()
+                      : string.Empty;
+            //Get Controller
+            string controller = filterContext.RouteData.Values["controller"].ToString();
+            
+            //Login Url
+            string loginUrl = string.Format("~/{0}/{1}/Login", area, controller);
+
+            filterContext.Result = new RedirectResult(string.Concat(loginUrl, "?returnUrl=", filterContext.HttpContext.Request.Url.PathAndQuery));
+        }
+    }
+}
