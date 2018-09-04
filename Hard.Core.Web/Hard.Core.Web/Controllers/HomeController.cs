980316c2e40e5aee9a6ec66d7e9336055200bb20
Edmund Smith <smithed@ROADS.VIC.GOV.AU>
2018-09-04 16:06:31 +1000

    Add ASP.Net Core sample.

---
+++ Hard.Core.Web/Hard.Core.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Hard.Core.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hard.Core.Web.Controllers
+{
+    
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            //ViewData["Message"] = "Your contact page.";
+
+            var sc = new ServiceCollection();
+            sc.AddDataProtection();
+            var svc = sc.BuildServiceProvider();
+            IDataProtectionProvider obj = svc.GetDataProtectionProvider();
+            IDataProtector p = obj.CreateProtector("Test v1");
+            ViewData["Message"] = p.Protect("Your contact page.");
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
