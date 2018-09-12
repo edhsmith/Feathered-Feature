14c80fd07a18f331adb23630237921dd32194d36
Edmund Smith <smithed@ROADS.VIC.GOV.AU>
2018-09-12 14:43:09 +1000

    Bounce! the Encryption.

---
+++ Common/Bouncy Castle/bccrypto-csharp-1.8.3/Samples/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string text = "This is a test message!_________________";
+            string keyStr = "12345678901234567890123456789012";
+
+            string str = Encrypt(keyStr, text);
+            string s = Decrypt(keyStr, str);
+            Console.WriteLine(s);
+
+        }
+
+
+       static IBlockCipher engine = new ThreefishEngine( ThreefishEngine.BLOCKSIZE_256);
+
+
+
+        public static string Encrypt(String keys, string clearText)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(keys);
+            byte[] ptBytes = Encoding.ASCII.GetBytes(clearText);
+            BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(engine));
+            cipher.Init(true, new KeyParameter(key));
+            byte[] rv = new byte[cipher.GetOutputSize(ptBytes.Length)];
+            int tam = cipher.ProcessBytes(ptBytes, 0, ptBytes.Length, rv, 0);
+            try
+            {
+                cipher.DoFinal(rv, tam);
+            }
+            catch (Exception ce)
+            {
+                //ce.printStackTrace();
+            }
+            return Convert.ToBase64String(rv);
+        }
+
+        public static string Decrypt(String key2, string cryptedText)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(key2);
+            byte[] cipherText = Convert.FromBase64String(cryptedText);
+            BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(engine));
+            
+            cipher.Init(false, new KeyParameter(key));
+            byte[] rv = new byte[cipher.GetOutputSize(cipherText.Length)];
+            int tam = cipher.ProcessBytes(cipherText, 0, cipherText.Length, rv, 0);
+            try
+            {
+                cipher.DoFinal(rv, tam);
+            }
+            catch (Exception ce)
+            {
+                //ce.printStackTrace();
+            }
+            return Encoding.ASCII.GetString(rv).Trim();
+        }
+
+    }
+}
---
+++ Common/Bouncy Castle/bccrypto-csharp-1.8.3/Samples/ThreeFish.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace Samples
+{
+    public class ThreeFish256 : ICryptoTransform
+    {
+        protected IBlockCipher engine;
+
+        public ThreeFish256()
+        {
+            engine = new ThreefishEngine(ThreefishEngine.BLOCKSIZE_256);
+        }
+
+
+        public int InputBlockSize
+        {
+            get
+            {
+                return engine.GetBlockSize();
+            }
+        }
+
+        public int OutputBlockSize => throw new NotImplementedException();
+
+        public bool CanTransformMultipleBlocks
+        {
+            get { return false; }
+        }
+
+        public bool CanReuseTransform { get { return false; } }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
